2fac0efff4fa0c22a33dca73073a8f856f804ea7
Terrell Terry <terrell.g.terry@gmail.com>
2023-10-19 17:04:11 -0700

    Adding readme back into project

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,13 +2,13 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/Skybox_P.glb?v=1697750991341");
+needle_exported_files.push("assets/Skybox_P.glb?v=1697753710488");
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1697750991341");
+		needleEngine.setAttribute("hash", "1697753710488");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
